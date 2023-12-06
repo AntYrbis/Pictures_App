0e4242ecb698053ce4abefbc60b46809e95c3486
AntYrbis <115887364+AntYrbis@users.noreply.github.com>
2023-12-06 13:10:43 +0100

    fixed description, modified layout a bit

--- public/mur.js
+++ public/mur.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Récupération du bouton
-    const layoutButton = document.querySelector('.button_layout');
-  
+    const layoutButton = document.getElementById('button_layout');
+    const imagesContainer = document.querySelector('.imgs_container'); // Remplacez avec votre sélecteur approprié
+
     // Fonction pour changer le layout des photos
     function changeLayout() {
-        const images = document.querySelectorAll('.img_cell');
-        images.forEach(image => {
-        let computedStyle = window.getComputedStyle(image);
-        let currentLayout = computedStyle.getPropertyValue('display');
-        if (currentLayout === 'inline') {
-            image.style.display= 'block';
-        } else {
-            image.style.display = 'inline';
-        }
-});
-}
+        // Ajoute ou supprime la classe 'single-column' selon l'état actuel
+        imagesContainer.classList.toggle('single-column');
+    }
+
     // Ajout des événements qui déclenchent les fonctions 
     layoutButton.addEventListener('click', changeLayout);
-  
+
     console.log('mur.js a été exécuté');
-  });
+});
